45a5ba9a2f593ebd851c12ac01d90052623daf59
ASO1801033 <1801033@s.asojuku.ac.jp>
2019-12-16 16:27:15 +0900

    プロジェクト作成完了

--- app/src/main/java/jp/ac/asojuku/s/revquizsound/MainActivity.kt
+++ app/src/main/java/jp/ac/asojuku/s/revquizsound/MainActivity.kt
@@ -1,12 +1,45 @@
 package jp.ac.asojuku.s.revquizsound
 
+import android.content.Intent
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.SoundPool
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var soundPool: SoundPool
+    private var soundResId = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //出題くんボタンを押したらQuestActivityへ遷移する
+        questButton.setOnClickListener {
+            val intent = Intent(this, QuestActivity::class.java)
+            startActivity(intent)
+        }
+
+        ansButton.setOnClickListener {
+            soundPool.play(
+                soundResId, 1.0f, 1.0f, 0, 0, 1.0f
+            )
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        soundPool = SoundPool(2, AudioManager.STREAM_ALARM, 0)
+        soundResId = soundPool.load(this, R.raw.anssound, 1)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        soundPool.release()
     }
 }
--- app/src/main/java/jp/ac/asojuku/s/revquizsound/QuestActivity.kt
+++ app/src/main/java/jp/ac/asojuku/s/revquizsound/QuestActivity.kt
@@ -1,12 +1,85 @@
 package jp.ac.asojuku.s.revquizsound
 
+import android.content.Intent
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.media.SoundPool
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_quest.*
 
 class QuestActivity : AppCompatActivity() {
+    private lateinit var soundPool: SoundPool
+    private var misssound = 0
+    private var corsound = 0
+    private var quizsound = 0
+    private var clapsound = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quest)
+
+        //解答くんボタンを押したらQuestActivityへ遷移する
+        answerButton.setOnClickListener { finish() }
+
+        soundPool =
+            if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                //OSのLOLLIPOPより古い端末のとき
+                SoundPool(
+                    2, //
+                    AudioManager.STREAM_ALARM, //オーディオの種類
+                    0) //音源品質、現在未使用。初期値の0を指定
+            }else {
+                //新しい端末のとき
+                val audioAttributes = AudioAttributes
+                    .Builder()
+                    .setUsage(AudioAttributes.USAGE_ALARM)
+                    .build()
+
+                //オーディオ設定を使ってSoundPoolのインスタンスを作る
+                SoundPool.Builder()
+                    .setMaxStreams(1) //同時音源数(1)
+                    .setAudioAttributes(audioAttributes)
+                    .build() //ビルド
+            }
+
+        //misssoundをロードしておく
+        misssound = soundPool.load(this, R.raw.misssound, 1)
+
+        //間違いボタンが押された時
+        missButton.setOnClickListener{
+            //play(ロードしたID, 左音量, 右音量, 優先度, ループ, 再生速度)
+            soundPool.play(misssound, 1.0f, 1.0f, 0, 0, 1.0f)
+        }
+
+        //corsoundをロードしておく
+        corsound = soundPool.load(this, R.raw.corsound, 1)
+
+        //正解ボタンが押された時
+        corButton.setOnClickListener {
+            //play(ロードしたID, 左音量, 右音量, 優先度, ループ, 再生速度)
+            soundPool.play(corsound, 1.0f, 1.0f, 0, 0, 1.0f)
+        }
+
+        //quizsoundをロードしておく
+        quizsound = soundPool.load(this, R.raw.quizsound, 1)
+
+        //出題ボタンが押された時
+        quizButton.setOnClickListener {
+            //play(ロードしたID, 左音量, 右音量, 優先度, ループ, 再生速度)
+            soundPool.play(quizsound, 1.0f, 1.0f, 0, 0, 1.0f)
+        }
+
+        //corsoundをロードしておく
+        clapsound = soundPool.load(this, R.raw.clapsound, 1)
+
+        //拍手ボタンが押された時
+        clapButton.setOnClickListener {
+            //play(ロードしたID, 左音量, 右音量, 優先度, ループ, 再生速度)
+            soundPool.play(clapsound, 1.0f, 1.0f, 0, 0, 1.0f)
+        }
     }
 }
